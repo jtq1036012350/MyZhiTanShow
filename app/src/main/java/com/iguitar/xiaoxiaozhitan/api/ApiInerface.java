1b2a0736e73848250a7810af4dd5bdc27842d50c
jtq@1036012350 <JTQc5235>
2017-09-27 09:38:48 +0800

    添加注释

--- app/src/main/java/com/iguitar/xiaoxiaozhitan/api/ApiInerface.java
+++ app/src/main/java/com/iguitar/xiaoxiaozhitan/api/ApiInerface.java
@@ -17,21 +17,27 @@ import retrofit2.http.GET;
  */
 
 public interface ApiInerface {
+    //语音模块
     @GET("Server/VoiceData.txt")
     Call<List<MyConversionBean>> getVoiceReturn();
 
+    //版本信息and测试网络
     @GET("Update/AppVersion.txt")
     Call<Object> getVersionReturn();
 
+    //用户信息
     @GET("Server/User.txt")
     Call<UserReturnBean> getLoginInfo();
 
+    //学习中心
     @GET("Server/Study.txt")
     Call<List<StudyJavaBean>> getStudyInfo();
 
+    //视频页顶部
     @GET("Server/VideoTop.txt")
     Call<List<MainListJavaBean>> getVideoTopInfo();
 
+    //视频页底部
     @GET("Server/VideoBottom.txt")
     Call<List<VideoBottomBean>> getVideoBottomInfo();
 }
