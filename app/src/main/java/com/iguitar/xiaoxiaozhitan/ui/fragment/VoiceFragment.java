041abea1e278ef1ddd04d321811ad089b18765d3
jtq(江天麒) <JTQc5235>
2019-09-25 16:49:59 +0800

    新增

--- app/src/main/java/com/iguitar/xiaoxiaozhitan/ui/fragment/VoiceFragment.java
+++ app/src/main/java/com/iguitar/xiaoxiaozhitan/ui/fragment/VoiceFragment.java
@@ -74,6 +74,7 @@ public class VoiceFragment extends BaseFragment {
         }
     }
 
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
